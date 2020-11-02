5a26304fcb7f2ce857026b36f0f3aca370d85152
Ryota09 <n.Ryo950911@gmail.com>
2020-11-02 22:49:50 +0900

    10heroku_error1

--- db/migrate/20201003062339_create_newses.rb
+++ db/migrate/20201003062339_create_newses.rb
@@ -1,10 +1,11 @@
-class CreateNewses < ActiveRecord::Migration[5.2]
+class CreateNews < ActiveRecord::Migration[5.2]
   def change
     create_table :news do |t|
       t.integer :user_id
       t.string :title
       t.text :body
       t.string :image_id
+      
 
       t.timestamps
     end
